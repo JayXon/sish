dc7bd39dd4725579510addefa90c897332c9f3b8
JayXon <jayxon@gmail.com>
2014-12-07 16:56:29 -0500

    Add support for arguments

--- shell.c
+++ shell.c
@@ -3,17 +3,49 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#include <ctype.h>
 #include <err.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+char **
+split_args(char *args)
+{
+    int argc = 2;
+    for (int i = 2; args[i]; i++)
+        if ((args[i - 1] == ' ' || args[i - 1] == '\t') && (args[i] != ' ' && args[i] != '\t'))
+            argc++;
+
+    char **argv = malloc(argc * sizeof(char *));
+
+    argc = 0;
+    const char *delim = " \t";
+    for (char *token = strtok(args, delim); token; token = strtok(NULL, delim))
+        argv[argc++] = token;
+    argv[argc] = NULL;
+
+    return argv;
+}
+
+char *
+skip_space(char *s)
+{
+    while (*s == ' ' || *s == '\t')
+        s++;
+    return s;
+}
 
 void
 execute_command(char *command, bool tracing)
 {
+    command = skip_space(command);
     if (tracing)
         fprintf(stderr, "+ %s\n", command);
+
+    char **argv = split_args(command);
 
     pid_t pid = fork();
     if (pid < 0) {
@@ -22,9 +54,10 @@ execute_command(char *command, bool tracing)
     }
 
     if (pid == 0) {
-        execlp(command, command, NULL);
+        execvp(command, argv);
         err(127, "%s", command);
     }
+    free(argv);
 
     int status;
     if (waitpid(pid, &status, 0) == -1)
